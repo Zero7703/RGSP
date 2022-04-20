09be8544df790d4c5528d4624d75fa4d99ac8168
jif <fengji@longshine.com>
2022-04-20 12:18:01 +0800

    hik  基础配置及jar包依赖提交

---
+++ rg-thirdpart-service/src/main/java/com/rg/thirdpart/hik/config/RunnerConfig.java
@@ -0,0 +1,64 @@
+package com.rg.thirdpart.hik.config;
+
+import com.hikvision.artemis.sdk.config.ArtemisConfig;
+import com.hikvision.artemis.sdk.constant.Constants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ *
+ * @author Harry
+ * @date 2020/8/10 20:41
+ */
+@Configuration
+public class RunnerConfig {
+
+  public static  final String ARTEMIS_PATH = "/artemis";
+
+  /**
+   * 请求协议
+   */
+  private static String protocol;
+
+  @Value("${hikvison.iot.host:127.0.0.1:8443}")
+  private  void setHost( String host ) {
+    ArtemisConfig.host = host;
+    Constants.DEFAULT_TIMEOUT = 10000;
+  }
+
+  /**
+   * 设置超时，默认是10秒
+   * @param minSeconds
+   */
+  @Value("${hikvison.iot.timeout:10000}")
+  private  void setTimeOut( Integer minSeconds ) {
+    Constants.DEFAULT_TIMEOUT = minSeconds;
+  }
+
+  @Value("${hikvison.iot.appKey:123456}")
+  private  void setAppKey( String appKey ) {
+    ArtemisConfig.appKey = appKey;
+  }
+  @Value("${hikvison.iot.appSecret:123456}")
+  private  void setAppSecret( String appSecret ) {
+    ArtemisConfig.appSecret = appSecret;
+  }
+
+  @Value("${hikvison.iot.protocol:https}")
+  private void setProtocol( String protocol ) {
+    RunnerConfig.protocol = protocol;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public  static  String  getProtocol(){
+    return RunnerConfig.protocol.concat("://");
+  }
+
+  public static String getArtemisPath() {
+    return ARTEMIS_PATH;
+  }
+}
